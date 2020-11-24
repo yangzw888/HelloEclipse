75356c530cc2cf4de74cce862acaed45b278e564
ZhiweiYang <zyang7@unb.ca>
2020-11-24 01:47:47 -0400

    HelloApp V2

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Wei");
+		System.out.println("Hello ZhiweiYang");
 	}
 
 }
